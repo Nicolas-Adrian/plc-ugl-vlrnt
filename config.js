593597efd4a4ccacb00701426345b081faa372bf
Nicolas Adrian <adriannicolas1311@gmail.com>
2026-05-09 16:36:05 -0300

    Update SCOREBOARD_APPS_SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Cole aqui a URL do Web App publicado no Google Apps Script.
 // Exemplo:
 // window.SCOREBOARD_APPS_SCRIPT_URL = "https://script.google.com/macros/s/SEU_ID/exec";
-window.SCOREBOARD_APPS_SCRIPT_URL = "";
+window.SCOREBOARD_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwf0yhCJ6Lu3FC-IqgNoyv8LYI6H2Cdbo4zKgxAAFmb7PWDmJzHXWJpr_yDP69aZt8-jQ/exec";
